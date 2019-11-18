942ec7c858524aba01cd301ea4e6ccb66bb5ba76
Torstein Solheim Ølberg <mr.torstein@gmail.com>
2019-11-18 14:53:05 +0100

    Auto stash before merge of "master" and "origin/master"

--- Scripts/Ising.h
+++ Scripts/Ising.h
@@ -25,6 +25,9 @@ class Ising {
         void Metropolis(int);
         void SpesificHeat();
         void Susceptibility();
+        /*void MeanEnergy();
+        void MeanMagnetization();
+        void Susceptibility();*/
 };
 
 #endif //ISING_H
